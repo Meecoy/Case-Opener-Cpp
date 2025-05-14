df86c402b1eb3e268f05982531c486d394d7d25f
DominikZajac <d.zajac.tp23@zsme.tarnow.pl>
2025-05-14 20:02:13 +0100

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,316 +1,118 @@
-#include "raylib.h"
+#include "libs/raylib/src/raylib.h"
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 
-// ========================================
-// IF U ON KNO!
-// "override" overriding a fucntion from parent's class
-// "virtual" function can be overridden later on
-// "auto" get type of something
-// ========================================
-
-// These should be in some kind of settings / utils / consts file
-// cosnts (settings)
 const int WIN_W = 800;
 const int WIN_H = 600;
-const char* TITLE = "Raylib Window";
 const int FPS = 60;
 
-enum class Screen {
-    TITLE,
-    CASE
+const float ITEM_WIDTH = 128.0f;
+const float ITEM_HEIGHT = 128.0f;
+const float SPACING = 150.0f;
+const int TOTAL_ITEMS = 100;
+const int WINNING_INDEX = 78;
+const float ROLL_DURATION = 4.5f;
+
+struct CaseItem {
+    Texture2D texture;
+    int index;
+
+    CaseItem(Texture2D tex, int idx) : texture(tex), index(idx) {}
+
+    void Draw(float scrollOffset) {
+        float x = index * SPACING - scrollOffset;
+        float y = 400;
+        Rectangle dst = { x, y, ITEM_WIDTH, ITEM_HEIGHT };
+        DrawRectangleRec(dst, { 100, 100, 100, 100 });
+        DrawTexturePro(texture, { 0, 0, (float)texture.width, (float)texture.height }, dst, { 0, 0 }, 0, WHITE);
+    }
 };
 
-class App {
+// Easing function for smooth scroll (ease out cubic)
+float EaseOutCubic(float t) {
+    return 1 - pow(1 - t, 3);
+}
 
-    public:
-        App(int width, int height, const char* title);
-        ~App();
-    
-        void run();
-    
-    private:
-        int winW;
-        int winH;
-        const char* title;
-        Screen currentScreen;
-
-        std::vector<Sprite*> visibleGroup;
-    
-        void update();
-        void render();
-    
-        void initTitleScreen();
-        void updateTitleScreen();
-        void renderTitleScreen();
-        void unloadTitleScreen();
-    
-        void initCaseScreen();
-        void updateCaseScreen();
-        void renderCaseScreen();
-        void unloadCaseScreen();
-    };
-
- 
-App::App(int width, int height, const char* title)
-    : winW(width), winH(height), title(title), currentScreen(Screen::TITLE) {
-    InitWindow(winW, winH, title);
+int main() {
+    InitWindow(WIN_W, WIN_H, "Case Opening");
     SetTargetFPS(FPS);
-    initTitleScreen();
-}
+    std::srand((unsigned int)std::time(nullptr));
 
-App::~App() {
-    CloseWindow();
-}
-
-void App::run() {
-    while (!WindowShouldClose()) {
-        update();
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        render();
-
-        EndDrawing();
-    }
-}
-
-void App::update() {
-    switch (currentScreen) {
-        case Screen::TITLE: updateTitleScreen(); break;
-        case Screen::CASE: updateCaseScreen(); break;
-        default: break;
-    }
-}
-
-void App::update() {
-    switch (currentScreen) {
-        case Screen::TITLE: renderTitleScreen(); break;
-        case Screen::CASE: renderCaseScreen(); break;
-        default: break;
-    }
-}
-
-void App::initTitleScreen() {
-    visibleGroup.clear();
-}
-
-void App::initCaseScreen() {
-    visibleGroup.clear();
-}
-
-void App::unloadTitleScreen() {
-    for (Sprite* s : visibleGroup)
-    {
-        delete s;
-    }
-
-    // ??? ==================================
-    // for (auto& texture : skinTextureDB)
-    // {
-    //     UnloadTexture(texture);
-    // }
-}
-
-void App::unloadCaseScreen() {
-    for (Sprite* s : visibleGroup)
-    {
-        delete s;
-    }
-}
-
-// Base sprite class
-class Sprite
-{
-    public:
-        virtual void update(float dt) {}
-        virtual void draw() {}
-        virtual bool shouldRemove() { return false; }
-        virtual ~Sprite() {}
-};
-
-// Item sprite
-class Item : public Sprite
-{
-    public:
-        Vector2 pos;
-        Vector2 size;
-        Texture2D texture;
-        Color tint;
-        Rectangle rect;
-        Color rect_color;
-    
-        Item(Vector2 p, Vector2 s, Texture2D t, Color c)
-        {
-            pos = p;
-            size = s;
-            texture = t;
-            tint = c;
-            rect = { pos.x, pos.y, size.x, size.y };
-            rect_color = { 100, 100, 100, 100 };
-        }
-    
-        void update(float dt) override
-        {
-            // pass
-            // frick c++
-        }
-    
-        void draw() override
-        {
-            DrawRectangleRec(rect, rect_color);
-            DrawTextureEx(texture, pos, 0, 0.25f, tint);
-        }
-};
-
-// Case item sprite
-class CaseItem : public Sprite
-{
-    public:
-        Vector2 pos;
-        Vector2 size;
-        Texture2D texture;
-        Color tint;
-        Rectangle rect;
-        Color rect_color;
-        Vector2 vel;
-    
-        CaseItem(Vector2 p, Vector2 s, Texture2D t, Color c)
-        {
-            pos = p;
-            size = s;
-            texture = t;
-            tint = c;
-            rect = { pos.x, pos.y, size.x, size.y };
-            rect_color = { 100, 100, 100, 100 };
-            vel = { 150, 0 };
-        }
-    
-        void update(float dt) override
-        {
-            pos.x += vel.x * dt;
-            rect.x = pos.x;
-        }
-    
-        void draw() override
-        {
-            DrawRectangleRec(rect, rect_color);
-            DrawTextureEx(texture, pos, 0, 0.25f, tint);
-        }
-    
-        bool shouldRemove() override
-        {
-            return rect.x > WIN_W;
-        }
-    
-        bool isStopped()
-        {
-            return vel.x == 0;
-        }
-};
-
-// WORK IN PROGRESS =============================
-// int main() {
-//     App app(WIN_W, WIN_H, TITLE);
-//     app.run();
-
-//     return 0;
-// }
-
-int main()
-{
-    InitWindow(WIN_W, WIN_H, TITLE);
-
-    SetTargetFPS(FPS);
-
-    // Create skin's textures database
-    Texture2D skinTextureDB[] =
-    {
+    std::vector<Texture2D> textureDB = {
         LoadTexture("Cases/polityczka/Skins/Chanuka.png"),
-        LoadTexture("Cases/polityczka/Skins/Skapiec.png"),
+        LoadTexture("Cases/polityczka/Skins/Skąpiec.png"),
         LoadTexture("Cases/polityczka/Skins/GierekCukierek.png"),
         LoadTexture("Cases/polityczka/Skins/Dewiant.png"),
         LoadTexture("Cases/polityczka/Skins/11l9(premium).png"),
-        LoadTexture("Cases/polityczka/Skins/Cziaa.png"),
+        LoadTexture("Cases/polityczka/Skins/Cziłała.png"),
         LoadTexture("Cases/polityczka/Skins/NiemieckiAgent.png"),
         LoadTexture("Cases/polityczka/Skins/StalowaBrzoza.png"),
+        LoadTexture("Cases/polityczka/Skins/RekaWolnegoRynku.png")
     };
-    const Texture2D drawnSkinTexture = skinTextureDB[0];
-    
-    // Sprite groups
-    std::vector<Sprite*> visibleGroup;
 
-    // Random seed
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::vector<CaseItem> items;
+    Texture2D winningTexture = textureDB[0];
 
+    float scrollOffset = 0.0f;
+    float targetOffset = 0.0f;
+    float elapsedTime = 0.0f;
+    bool rolling = false;
+    bool resultShown = false;
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         float dt = GetFrameTime();
 
+        if (IsKeyPressed(KEY_SPACE)) {
+            items.clear();
+            for (int i = 0; i < TOTAL_ITEMS; i++) {
+                Texture2D tex = (i == WINNING_INDEX) ? textureDB[0] : textureDB[std::rand() % textureDB.size()];
+                items.emplace_back(tex, i);
+                if (i == WINNING_INDEX) winningTexture = tex;
+            }
+
+            // Calculate how far we need to scroll to center the winning item
+            targetOffset = WINNING_INDEX * SPACING - (WIN_W / 2.0f - ITEM_WIDTH / 2.0f);
+            scrollOffset = 0.0f;
+            elapsedTime = 0.0f;
+            rolling = true;
+            resultShown = false;
+        }
+
+        if (rolling) {
+            elapsedTime += dt;
+            float t = elapsedTime / ROLL_DURATION;
+            if (t > 1.0f) {
+                t = 1.0f;
+                rolling = false;
+                resultShown = true;
+            }
+
+            scrollOffset = EaseOutCubic(t) * targetOffset;
+        }
+
         BeginDrawing();
+        ClearBackground({ 25, 25, 25, 255 });
 
-        ClearBackground(Color{25, 25, 25, 255});
-
-        // Update sprites
-        for (int i = 0; i < visibleGroup.size(); )
-        {
-            visibleGroup[i]->update(dt);
-
-            // Remove if out of screen
-            if (visibleGroup[i]->shouldRemove())
-            {
-                delete visibleGroup[i];
-                visibleGroup.erase(visibleGroup.begin() + i);
-            } else
-            {
-                i++;
-            }
+        for (auto& item : items) {
+            item.Draw(scrollOffset);
         }
 
-        // Add case items when SPACE is pressed
-        // if (IsKeyPressed(KEY_SPACE)) // imo to powinno dzialac, ale nie wiem
-        if (IsKeyPressed(32)) // ???
-        {
-            for (int i = 0; i < 82; i++)
-            {
-                Texture2D texture = (i == 78) ? drawnSkinTexture : skinTextureDB[rand() % 8];
-                CaseItem* ci = new CaseItem({ WIN_W + i * -150.0f, 400 }, { 128, 128 }, texture, { 200, 200, 200, 64 });
-                visibleGroup.push_back(ci);
-            }
-        }
+        DrawLine(WIN_W / 2, 0, WIN_W / 2, WIN_H, RED);
 
-        // Check if 5th sprite is a CaseItem and has stopped, then add Item
-        if (visibleGroup.size() > 4)
-        {
-            // "visibleGroup[4]" is a pointer
-            // So we're using dynamic_cast to conver that pointer to a pointer to a "CaseItem"
-            CaseItem* ci = dynamic_cast<CaseItem*>(visibleGroup[4]);
-            if (ci && ci->isStopped())
-            {
-                Item* item = new Item({ 150, 150 }, { 128, 128 }, drawnSkinTexture, { 200, 200, 200, 64 });
-                visibleGroup.push_back(item);
-            }
+        if (resultShown) {
+            DrawText("You got:", 50, 50, 24, WHITE);
+            Rectangle dst = { 200, 30, ITEM_WIDTH, ITEM_HEIGHT };
+            DrawRectangleRec(dst, { 80, 80, 80, 100 });
+            DrawTexturePro(winningTexture, { 0, 0, (float)winningTexture.width, (float)winningTexture.height },
+                           dst, { 0, 0 }, 0.0f, WHITE);
         }
 
         EndDrawing();
     }
 
-    // Delete, unload everythinh
-    for (Sprite* s : visibleGroup)
-    {
-        delete s;
-    }
-
-    for (auto& texture : skinTextureDB)
-    {
-        UnloadTexture(texture);
-    }
-
+    for (auto& tex : textureDB) UnloadTexture(tex);
     CloseWindow();
-
     return 0;
 }
